978e9616d828bcea69d68a05987d7b1bee4cdc83
zeyuan629 <45072335+zeyuan629@users.noreply.github.com>
2021-02-22 19:40:08 +0800

    Update The crawler code.py

--- The crawler code.py
+++ The crawler code.py
@@ -4,7 +4,7 @@ headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3775.400 QQBrowser/10.6.4209.400',
 }
 cursor='0'
-source='1613549142009'
+source='1613993209527'
 n=12688//10
 for i in range(0,n):
     url = 'https://coral.qq.com/article/5963120294/comment/v2?callback=_article5963120294commentv2&orinum=10&oriorder=o&pageflag=1&cursor='+cursor+'&scorecursor=0&orirepnum=2&reporder=o&reppageflag=1&source=1&_='+source
